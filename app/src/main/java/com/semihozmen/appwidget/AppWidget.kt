b1b989bcfadbe3e3c710684fa251b849e63f4e90
ozmensemihh <ozmensemih90@gmail.com>
2023-07-08 06:37:49 +0300

    Initial commit

---
+++ app/src/main/java/com/semihozmen/appwidget/AppWidget.kt
@@ -0,0 +1,54 @@
+package com.semihozmen.appwidget
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+/**
+ * Implementation of App Widget functionality.
+ */
+class AppWidget : AppWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray
+    ) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetId: Int
+) {
+    val receiver = Intent(context, SoundReceiver::class.java)
+    receiver.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, appWidgetId);
+    val pendingIntent = PendingIntent.getBroadcast(
+        context,
+        0,
+        receiver,
+        PendingIntent.FLAG_IMMUTABLE
+    );
+
+    val views = RemoteViews(context.getPackageName(),
+        R.layout.app_widget)
+    views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent);
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
---
+++ app/src/main/java/com/semihozmen/appwidget/SoundReceiver.kt
@@ -0,0 +1,48 @@
+package com.semihozmen.appwidget
+
+import android.appwidget.AppWidgetManager
+import android.content.BroadcastReceiver
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
+import android.media.AudioManager
+import android.os.Build
+import android.widget.RemoteViews
+import androidx.annotation.RequiresApi
+
+class SoundReceiver  : BroadcastReceiver () {
+    @RequiresApi(Build.VERSION_CODES.S)
+    override fun onReceive(context: Context?, p1: Intent?) {
+
+        val views = RemoteViews(context!!.packageName, R.layout.app_widget)
+        val audioManager:AudioManager = context.getSystemService(AudioManager::class.java)
+        val appWidgetManager = AppWidgetManager.getInstance(context);
+        appWidgetManager.updateAppWidget(
+            ComponentName(context,AppWidget::class.java),views)
+
+            when(audioManager.ringerMode){
+            AudioManager.RINGER_MODE_NORMAL ->{
+                audioManager.ringerMode = AudioManager.RINGER_MODE_VIBRATE
+                views.setTextViewText(R.id.appwidget_text, "Aç")
+                appWidgetManager.updateAppWidget(
+                    ComponentName(context,AppWidget::class.java),views)
+            }
+
+            AudioManager.RINGER_MODE_SILENT ->{
+                audioManager.ringerMode = AudioManager.RINGER_MODE_VIBRATE
+                views.setTextViewText(R.id.appwidget_text, "Kapat")
+                appWidgetManager.updateAppWidget(
+                    ComponentName(context,AppWidget::class.java),views)
+            }
+
+                AudioManager.RINGER_MODE_VIBRATE ->{
+                    audioManager.ringerMode = AudioManager.RINGER_MODE_NORMAL
+                    views.setTextViewText(R.id.appwidget_text, "Kapat")
+                    appWidgetManager.updateAppWidget(
+                        ComponentName(context,AppWidget::class.java),views)
+                }
+        }
+
+    }
+}
